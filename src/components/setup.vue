4ecd8da72caba7c044b8679b84807c02d0f693a9
davide.cirone <davide.cirone@weroad.it>
2021-07-05 11:34:41 +0200

    first commit

---
+++ src/components/setup.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="increaseCount">count is: {{ state.count }}</button>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 })
+
+const increaseCount = () => {
+  state.count++
+}
+</script>
